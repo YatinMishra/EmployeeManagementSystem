74066a8f4ad0a5cb84c1bbf5d9c3743f53603bbb
Yatin Mishra <Yatin Mishra@DESKTOP-PMPR6MA>
2021-03-11 13:59:12 +0530

    Third commit

--- src/main/java/com/cg/ems/controller/DepartmentController.java
+++ src/main/java/com/cg/ems/controller/DepartmentController.java
@@ -2,12 +2,17 @@ package com.cg.ems.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.ems.services.IDepartmentService;
+import com.capg.cba.entities.Customer;
 import com.cg.ems.entities.Department;
 
 @RestController
@@ -15,24 +20,37 @@ import com.cg.ems.entities.Department;
 public class DepartmentController {
 	@Autowired
 	private IDepartmentService departmentService;
+	//to get the details of all department
 	@GetMapping(value="/GetAllDepartment")
 	public List<Department> getAllDepartment()
 	{
 	return	this.departmentService.getAllDepartments();
 	}
+	//to get the information of all department by pagination
 	@GetMapping(value="/GetAllDepartmentByPage")
 	public List<Department> getAllDepartByPagination(String maxPage, String ofset)
 	{
 	return	this.departmentService.getAllDepartByPagination(maxPage, ofset);
 	}
+	//get the details of all department by count
 	@GetMapping(value="/GetAllDepartmentByCount")
 	public int getAllDepartCount()
 	{
 	return	this.departmentService.getAllDepartCount();
 	}
+	//to add the department
 	@PostMapping(value="/addDepartment")
     public	void addDepartment(String dname)
 	{
-		
+		departmentService.addDepartment(dname);
 	}
+	//to delete a department
+	@DeleteMapping(value = "/deleteCustomer/{cid}")
+	public ResponseEntity<String> deleteDepartment(@PathVariable Integer id)
+	{
+		int department=departmentService.getAllDepartCount();
+		departmentService.deleteDepartment(department);
+		return new ResponseEntity<String>("Department With ID :"+id+" Deleted Successfully",HttpStatus.OK);
+	}
+	
 }
--- src/main/java/com/cg/ems/repository/DepartmentRepository.java
+++ src/main/java/com/cg/ems/repository/DepartmentRepository.java
@@ -1,9 +1,43 @@
 package com.cg.ems.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cg.ems.entities.Department;
 
 public class DepartmentRepository implements IDepartmentRepository  {
 @Autowired
 ICustomDeptRepository repository;
+
+@Override
+public void addDepartment(String dname) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public List<Department> getAllDepartments() {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+@Override
+public List<Department> getAllDepartByPagination(String maxPage, String ofset) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+@Override
+public int getAllDepartCount() {
+	// TODO Auto-generated method stub
+	return 0;
+}
+
+@Override
+public boolean deleteDepartment(int id) {
+	// TODO Auto-generated method stub
+	return false;
+}
 
 }
